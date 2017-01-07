ae496b60415bf0cd8a9af700d15e757e2da20a02
Alexis BRAULT <alexis.brault@wanadoo.fr>
2017-01-07 16:46:31 +0100

    Nouvelle version de la montée en charge

--- esir.compilation/src/sprint2/ThreeAddressCode.java
+++ esir.compilation/src/sprint2/ThreeAddressCode.java
@@ -163,7 +163,7 @@ public class ThreeAddressCode {
 			tree.add(s, funList, treeLevel);
 		}
 		
-		System.out.println(tree);
+		//System.out.println(tree);
 	}
 
 	public int inlineExpression(GeneratorAddr generatorAddr, DefFun f) throws ThreeAddressCodeException {

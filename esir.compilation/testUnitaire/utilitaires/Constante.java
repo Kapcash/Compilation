4180413381874b1b67e456fd1c3a3e3b474f60f5
DylanBehetre <dylan.behetre@outlook.fr>
2017-01-10 16:01:52 +0100

    Mise à jour du projet en mode eclipse

--- esir.compilation/testUnitaire/utilitaires/Constante.java
+++ esir.compilation/testUnitaire/utilitaires/Constante.java
@@ -4,9 +4,8 @@ public class Constante {
 	
 	public final static String PATH_COMMANDE = "esir.compilation/testUnitaire/";
 	public final static String PATH_ECLIPSE = "testUnitaire/";
-	public final static String PATH = PATH_COMMANDE;
+	public final static String PATH = PATH_ECLIPSE;
 	
 	public final static boolean TEST_TRACE = (PATH.equals(PATH_COMMANDE));
 	public final static boolean DEBUG_TRACE = (PATH.equals(PATH_ECLIPSE));
-	
 }
